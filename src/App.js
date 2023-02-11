573e87511be1db28fd573ab6e0dec7fa8dbbfb4f
GomgomKim <dhflwlsjfzz@naver.com>
2023-02-11 19:01:19 +0900

    [Add Nav and Bootstrap] 1. Add react-bootstrap 2. Add Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import BookListTable from './pages/BookListTable'
+import { Navbar,Container,Nav,Card,Button} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
         <BookListTable />
     </div>
   );
